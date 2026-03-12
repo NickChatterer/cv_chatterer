6a4d2b5e5c3b37015055e7752ce6bfa5605aede2
Doniyor <dkuzybaev@yandex.ru>
2026-03-12 18:04:03 +0300

    change astro.config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,10 @@
 import { defineConfig } from 'astro/config';
 
-// Замени 'your-username' и 'your-repo' на свои данные
-// Например: site: 'https://chatterer.github.io', base: '/portfolio'
-// Если репозиторий называется username.github.io — base убери совсем
-// export default defineConfig({
-//   site: 'https://your-username.github.io',
-//   base: '/your-repo-name',
-// });
+
+export default defineConfig({
+  site: 'https://NickChatterer.github.io',
+  base: '/cv_chatterer',
+});
 
 
-export default defineConfig({});
+// export default defineConfig({});
